fec2545c27ec4733fb766663f27650029c00c364
Akhil <saiakil456@gmail.com>
2023-06-30 10:18:41 +0530

    change Url for one project

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,7 +12,7 @@ import "./work.css"
 const cardStore = [{ image: ipl, text: "IPL DASHBOARD", gitlink: "https://github.com/akhilsai2/ipldashboard-reactjs", applink: "https://ipldash.ccbp.tech" },
 { image: job, text: "JOBBY APP", gitlink: "https://github.com/akhilsai2/jobbyApp-reactjs", applink: "https://jobbyapp8.ccbp.tech" },
 { image: rps, text: "RPS GAME", gitlink: "https://github.com/akhilsai2/rpsgame-reactjs", applink: "https://RPSgame8.ccbp.tech" },
-{ image: trend, text: "ECOMMERCE APP", gitlink: "https://github.com/akhilsai2/E-Commerce-App", applink: "https://ecommerceTrends.ccbp.tech" },
+{ image: trend, text: "ECOMMERCE APP", gitlink: "https://github.com/akhilsai2/E-Commerce-App", applink: "https://nxttrendz8.ccbp.tech/products" },
 { image: watch, text: "YOUTUBE CLONE", gitlink: "https://github.com/akhilsai2/nxtwatch-reactjs", applink: "https://watchtube4.ccbp.tech" },
 ]
 const Work = () => {
